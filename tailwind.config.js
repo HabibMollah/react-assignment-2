bf3a2916a5e1a2a3906fe18c7cb30b86c2a289cc
HabibMollah <habib.prodev@gmail.com>
2023-10-02 20:04:17 +0600

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#363ae4",
+        accent: "#ececfb",
+      },
+    },
   },
   plugins: [],
 };
