9d3ed35df5fa3519f2d87a0c2a8005fb99473ab7
HabibMollah <habib.prodev@gmail.com>
2023-10-02 19:46:46 +0600

    add data and define type

---
+++ src/types/property.ts
@@ -0,0 +1,16 @@
+type Property = {
+  id: number;
+  name: string;
+  location: string;
+  isForRent: boolean;
+  isForSale: boolean;
+  rooms: number;
+  beds: number;
+  bathroom: number;
+  area: number;
+  price: number;
+  city: string;
+  isPopular: boolean;
+  imageURL: string;
+};
+export default Property;
